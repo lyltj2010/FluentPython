5fd345e1edd0479083933ddd9a295a0ff8d2c25a
yongle <lyuyongle@gmail.com>
2017-10-04 08:40:49 +0800

    new list from string

---
+++ 01ThePythonDataModel/list_from_string.py
@@ -0,0 +1,11 @@
+"""
+>>> suits
+['spades', 'diamonds', 'clubs', 'hearts']
+"""
+
+suits = 'spades diamonds clubs hearts'.split()
+
+
+if __name__ == "__main__":
+    import doctest
+    doctest.testmod()
