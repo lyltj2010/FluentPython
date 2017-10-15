5f35777642debf7b6c597cfd63c367ecbf03bc42
yongle <lyuyongle@gmail.com>
2017-10-15 09:33:59 +0800

    user defined callable class

---
+++ 05FirstClassFunctions/user_defined_callable.py
@@ -0,0 +1,30 @@
+"""
+Arbitrary Python objects can be made to behave like functions.
+Implementing a __call__ instance method is all it takes.
+>>> bingo = BingoCage([1, 1, 1])
+>>> bingo.pick() # what __call__ provides
+1
+>>> bingo()
+1
+"""
+import random
+
+
+class BingoCage:
+    def __init__(self, items):
+        self._items = list(items)  # build a local copy
+        random.shuffle(self._items)
+
+    def pick(self):
+        try:
+            return self._items.pop()
+        except IndexError:
+            raise LookupError('pick from empty BingoCage')
+
+    def __call__(self):
+        return self.pick()
+
+
+if __name__ == "__main__":
+    import doctest
+    doctest.testmod()
